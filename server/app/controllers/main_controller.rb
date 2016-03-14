c9b978ee3fad6b279f3057f7552339ab9daf927b
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-14 17:15:15 -0500

    Question/Answer on home.erb

--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -1,3 +1,7 @@
 MyApp.get "/" do
+  @first_question = Question.find_by_id(1)
+  @first_question.save
+  @answer_to_question_one = Answer.find_by_id(1)
+  @answer_to_question_one.save
   erb :"home"
 end
