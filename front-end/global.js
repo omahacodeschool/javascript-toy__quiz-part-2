907add806f048489d95e132013b650747cedc453
Kyle Sandage <ksandage08@gmail.com>
2016-03-14 15:02:06 -0500

    three questions show up

--- front-end/global.js
+++ front-end/global.js
@@ -1,21 +1,60 @@
 window.onload = function(){
 
-  startButton = document.getElementById("begin_button")
+  startButton = document.getElementById("begin_button");
+  var params = 1;
+  var correctAnswers = 0;
   startButton.addEventListener("click", function(){
 
     var request = new XMLHttpRequest();
-    var params = 1
-    request.open("GET", "http://localhost:9292/add/question/" + params)
+
+    request.open("GET", "http://localhost:9292/add/question/" + 1)
     request.addEventListener("load", function(event){
       var the_request = event.target;
       userResponse = prompt(the_request.responseText);
-      if(userResponse.toLowerCase() == "Array"){
+      if(userResponse.toLowerCase() == "array"){
         alert("Correct!");
+        correctAnswers++;
       }
       else{
         alert("Wrong!");
       };
     });
     request.send();
+    var request = new XMLHttpRequest();
+
+    request.open("GET", "http://localhost:9292/add/question/" + 2)
+    request.addEventListener("load", function(event){
+      var the_request = event.target;
+      userResponse = prompt(the_request.responseText);
+      if(userResponse.toLowerCase() == "objects"){
+        alert("Correct!");
+        correctAnswers++;
+      }
+      else{
+        alert("Wrong!");
+      };
+    });
+    request.send();
+    var request = new XMLHttpRequest();
+
+    request.open("GET", "http://localhost:9292/add/question/" + 3)
+    request.addEventListener("load", function(event){
+      var the_request = event.target;
+      userResponse = prompt(the_request.responseText);
+      if(userResponse.toLowerCase() == "the dom"){
+        alert("Correct!");
+        correctAnswers++;
+      }
+      else{
+        alert("Wrong!");
+      };
+    });
+    request.send();
+    alert("Game Over")
+    alert("You answered " + correctAnswers + " questions correctly out of 3.")
   });
 };
+
+
+
+
