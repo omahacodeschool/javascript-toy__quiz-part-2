155e51dbd9e5c59b5540e45fca9a282d0446b364
Ryan Newell <youcatastrophe@gmail.com>
2016-03-14 17:02:35 -0500

    Part 2, Phase 2 - Got first question to display

--- front-end/global.js
+++ front-end/global.js
@@ -1,19 +1,73 @@
 window.onload = function(){
 
-  
+// Initialization of variables
+  var currentQuestion = 0,
+     score = 0,
+     chose;
+
+  var submitter = document.getElementById('submitter');
+  var nextButton = document.getElementById("nextButton");
+  var begin = document.getElementById("beginButton");
+  var answerField = document.getElementById("answer");
+  var question = document.getElementById("question");    
+  var totalResult = document.getElementById("totalResult");  
+
+// sets up next/first question. Copied/pasted/modified code from part 1 phase 2
+// Still needs to be heavily modified
+
+  function nextQuestion() { 
+
+    begin.style.display = "none";
+
+    var node = document.getElementById('choices');
+    while (node.hasChildNodes()) {
+        node.removeChild(node.firstChild);
+    }
+
+      // (function(){
+      //   var ul = document.createElement('ul');
+      //   ul.setAttribute('id','choiceList');
+
+
+      //   document.getElementById('choices').appendChild(ul);
+      //   quiz[currentQuestion]['choices'].forEach(showChoices);
+
+      //   function showChoices(element, index, arr) {
+
+      //       var li = document.createElement('li');
+
+      //       ul.appendChild(li);
+      //       li.innerHTML = li.innerHTML + element;
+
+      //   }
+      // })();
+
+
+      submitter.style.display = "block";
+
+      answerField.style.display = "block";    
+
+    };  
+
+
+
+// user clicks "begin" button to start quiz. Code from Part 2, Phase 1 (modified)
+
   var begin = document.getElementById("beginButton");
 
 
   begin.addEventListener("click", function() {
     var request = new XMLHttpRequest();
-    request.open("GET", " http://localhost:9292/combined_question_and_answer/1")
+    request.open("GET", " http://localhost:9292/question/" + (currentQuestion +1));
 
     request.addEventListener("load", function() {
       var theRequest = event.target;
-      var answer = prompt(theRequest.responseText);
+      var questionText = theRequest.responseText;
+      question.innerHTML = questionText;
+      nextQuestion(); 
     });
 
     request.send();
   });
 
-};
+};  
--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -2,9 +2,12 @@ MyApp.get "/" do
   erb :"home"
 end
 
-MyApp.get "/combined_question_and_answer/:id" do 
+MyApp.get "/question/:id" do 
   @question = Question.find_by_id(params[:id])
-  @answers = @question.answers
-  @correctAnswer = @question.correct_Answer
-  erb :"question_answers"
+  erb :"question"
 end 
+
+MyApp.get "/answer/:id" do
+  @answers = Answer.where({"question_id" => params[:id]})
+  erb :"answers"
+end
