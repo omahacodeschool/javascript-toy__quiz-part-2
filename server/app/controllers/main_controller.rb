a5ca3536531a35f0219c5af2d946621b5e4955b7
Amy Wall <amymariewall@gmail.com>
2016-03-14 16:26:58 -0500

    Wrote controller action to get the text of a question and answer pair and render it on a view file. I got a lot of errors while doing this and I have no idea why. If you have time, go back and investigate. Also note that I ran ruby main.rb in command line while trying to debug this issue and it started a server. Figure out how that is different than running rackup.

--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -1,3 +1,10 @@
 MyApp.get "/" do
   erb :"home"
 end
+
+MyApp.get "/combined_question_and_answer/:id" do
+  @question = Question.find_by_id(params[:id])
+  @answers = @question.answers
+  @correct_answer = @question.correct_answer
+  erb :"question_answer_one"
+end
