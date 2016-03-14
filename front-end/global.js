c4cf87995123fed14c27d25fbfc13c182d671386
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-14 11:23:32 -0500

    first let's add a question before we try to load a question

--- front-end/global.js
+++ front-end/global.js
@@ -1,2 +1,11 @@
 window.onload = function(){
+  //i know we'll still need "q", our question counter variable
+  var q = 0
+  //and we'll still need to track our right_answers counter variable
+  var right_answers = 0
+
+  var begin = document.getElementById("begin_button");
+  begin.addEventListenter("click",function()) {
+    //load the first question with an XHR request
+  }
 };
