8dda7e8916de10913d0fb345f5804b926c555f89
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-14 18:04:34 -0500

    wip phase 1

--- front-end/global.js
+++ front-end/global.js
@@ -1,4 +1,13 @@
 window.onload = function(){
 
-  pr
+  var button = document.getElementById("begin_button")
+
+  button.addEventListener("click", function(){
+
+    var first_question = new XMLHttpRequest();
+
+    first_question.open("get", "http://localhost:9292");
+
+    first_question.send();
+  });
 };
