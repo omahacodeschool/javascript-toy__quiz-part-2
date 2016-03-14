4cdcb8b24a8dc4f8ed574314df73995f4ca4ef33
Amy Wall <amymariewall@gmail.com>
2016-03-14 16:49:03 -0500

    Added a listener to the begin button to trigger the creation of a Request Object. I also added an event listener onto that Request Object that triggers a function that displays the responseText of that Request Object in an alert after the Response is loaded.

--- front-end/global.js
+++ front-end/global.js
@@ -1,2 +1,32 @@
 window.onload = function(){
+
+  var begin_button = document.getElementById("begin_button");
+
+  begin_button.addEventListener("click", function() {
+
+
+
+  var request = new XMLHttpRequest();
+
+  request.open("GET", " http://localhost:9292/combined_question_and_answer/1");
+  // a request to the server running through http-server
+
+  request.addEventListener("load", function(event){
+    var the_request = event.target;
+    alert(the_request.responseText);
+    // responseText is a built-in method for request objects.
+  });
+
+  request.send();
+
+});
+
+
+
+
+
+
+
+
+
 };
