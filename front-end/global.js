80ddc5326c98bda996a2b0fd3a9fc4d66cfa4c3b
Kyle Sandage <ksandage08@gmail.com>
2016-03-14 16:52:32 -0500

    question

--- front-end/global.js
+++ front-end/global.js
@@ -3,58 +3,39 @@ window.onload = function(){
   startButton = document.getElementById("begin_button");
   var params = 1;
   var correctAnswers = 0;
+  var userResponse = "";
+
   startButton.addEventListener("click", function(){
 
     var request = new XMLHttpRequest();
-
-    request.open("GET", "http://localhost:9292/add/question/" + 1)
+    
+    request.open("GET", "http://localhost:9292/add/question/" + params);
     request.addEventListener("load", function(event){
       var the_request = event.target;
       userResponse = prompt(the_request.responseText);
-      if(userResponse.toLowerCase() == "array"){
-        alert("Correct!");
-        correctAnswers++;
-      }
-      else{
-        alert("Wrong!");
-      };
     });
     request.send();
-    var request = new XMLHttpRequest();
+    answer.send();
+  });
 
-    request.open("GET", "http://localhost:9292/add/question/" + 2)
-    request.addEventListener("load", function(event){
-      var the_request = event.target;
-      userResponse = prompt(the_request.responseText);
-      if(userResponse.toLowerCase() == "objects"){
-        alert("Correct!");
-        correctAnswers++;
-      }
-      else{
-        alert("Wrong!");
-      };
-    });
-    request.send();
-    var request = new XMLHttpRequest();
+  userResponse.addEventListener("click",function(){
 
-    request.open("GET", "http://localhost:9292/add/question/" + 3)
-    request.addEventListener("load", function(event){
-      var the_request = event.target;
-      userResponse = prompt(the_request.responseText);
-      if(userResponse.toLowerCase() == "the dom"){
-        alert("Correct!");
-        correctAnswers++;
-      }
-      else{
-        alert("Wrong!");
-      };
+    var answer = new XMLHttpRequest();
+
+    answer.open("GET", "http://localhost:9292/is_correct/"
+    +params+"/"+userResponse);
+    answer.addEventListener("load", function(event){
+      var is_correct = event.target;
+      alert(is_correct.responseText)
     });
-    request.send();
-    alert("Game Over")
-    alert("You answered " + correctAnswers + " questions correctly out of 3.")
   });
 };
 
 
 
-
+      // if(userResponse.toLowerCase() == "array"){
+      //   alert("Correct!");
+      //   correctAnswers++;
+      // }
+      // else{
+      //   alert("Wrong!");
--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -8,24 +8,9 @@ MyApp.get "/add/question/:id" do
   erb :"new_question"
 end
 
-MyApp.post "/add/question_to_database" do
-  #cool stuff
-  add = Question.new
-  add.content = params["question"]
-  add.save
+MyApp.get "/is_correct/:id/:answer" do
 
-  redirect "/"
+  erb :"answer"
 end
 
-MyApp.get "/add/answers" do
-  erb :"new_answer"
-end
 
-MyApp.post "/add/answer_to_database" do
-  #cool stuff
-  redirect "/add/answers"
-end
-
-MyApp.get "get/question" do
-  erb :"new_question"
-end
