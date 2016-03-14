4d7ff907d0e8713506c0e67a3fbb26417ab9cba5
Ryan Newell <youcatastrophe@gmail.com>
2016-03-14 12:53:11 -0500

    Testing to make sure I make the right connections

--- front-end/global.js
+++ front-end/global.js
@@ -1,2 +1,18 @@
 window.onload = function(){
+  var begin = document.getElementById("beginButton");
+
+
+  begin.addEventListener("click", function() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://localhost:9292/")
+
+    request.addEventListener("load", function() {
+      var theRequest = event.target;
+      alert(theRequest.responseText);
+    });
+
+    request.send();
+  });
+
+
 };
