aaa756848c8cf6c76d501b832569202a4a55e6d5
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-14 14:06:41 -0500

    Separated question request from choices

--- front-end/global.js
+++ front-end/global.js
@@ -3,32 +3,18 @@ window.onload = function(){
 var begin = document.getElementById('begin_button');
 
 begin.addEventListener("click", function() {
-  var qna_request = new XMLHttpRequest();
+  var question_request = new XMLHttpRequest();
 
-  qna_request.open("get", "http://localhost:9292/question_and_choices/1");
-  qna_request.addEventListener("load", function(event){
-    var the_qna_request = event.target;
-    prompt(the_qna_request.responseText);
+  question_request.open("get", "http://localhost:9292/question/1");
+  question_request.addEventListener("load", function(event){
+    var the_question_request = event.target;
+    alert(the_question_request.responseText);
 
   });
 
-qna_request.send();
+question_request.send();
 
 });
-
-
-// var request = new XMLHttpRequest();
-
-// request.open("GET", "http://localhost:8080/info");
-// // a request to the server running through http-server
-
-// request.addEventListener("load", function(event){
-//     var the_request = event.target;
-//     alert(the_request.responseText);
-//     // responseText is a built-in method for request objects.
-// });
-
-// request.send();
 
 
 
--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -2,7 +2,12 @@ MyApp.get "/" do
   erb :"home"
 end
 
-MyApp.get "/question_and_choices/:id" do
+MyApp.get "/question/:id" do
   @question_object = Question.find_by_id(params[:id])
-  erb :"/question_and_choices"
+  erb :"/question"
+end
+
+MyApp.get "/choices/:id" do
+  @question_object = Question.find_by_id(params[:id])
+  erb :"/choices"
 end
