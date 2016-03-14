665f9e40e810ca2a283eb90282a59babab8ea8e3
Amy Wall <amymariewall@gmail.com>
2016-03-14 18:55:15 -0500

    Added a new Request Object to send the user's answer and get back whether it was correct or incorrect.

--- front-end/global.js
+++ front-end/global.js
@@ -7,19 +7,33 @@ window.onload = function(){
 
 
   var request = new XMLHttpRequest();
-
+    //creates a new Request Object
   request.open("GET", " http://localhost:9292/combined_question_and_answer/1");
-  // a request to the server running through http-server
+    // specifies the path and http verb for the request, using the open method 
 
   request.addEventListener("load", function(event){
     var the_request = event.target;
-    alert(the_request.responseText);
+    var userAnswer = prompt(the_request.responseText);
     // responseText is a built-in method for request objects.
+
+    var request_answer = new XMLHttpRequest();
+
+    request_answer.open("GET", "http://localhost:9292/check_answer/1/" + userAnswer);
+
+    request_answer.addEventListener("load", function(event) {
+      var answer_request = event.target;
+      alert(answer_request.responseText);
+    });
+
+    request_answer.send();
+
   });
 
   request.send();
 
-});
+  });
+
+
 
 
 
--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -8,3 +8,13 @@ MyApp.get "/combined_question_and_answer/:id" do
   @correct_answer = @question.correct_answer
   erb :"question_answer_one"
 end
+
+MyApp.get "/check_answer/:id/:answer" do 
+  @question = Question.find_by_id(params[:id])
+  @correct_answer = @question.correct_answer
+  if @correct_answer[0].content == params[:answer]
+    return "CORRECT!"
+  else
+    return "INCORRECT!"
+  end
+end
