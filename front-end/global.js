f692543d5b442ca8e808e8147b39a71c0f8b7a10
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-14 15:57:41 -0500

    Points counter working

--- front-end/global.js
+++ front-end/global.js
@@ -4,20 +4,21 @@ var points_count = 0;
 var current_question = 0;
 var begin = document.getElementById('begin_button');
 var enter = document.getElementById('submitter');
+var points = document.getElementById('result');
 
 begin.addEventListener("click", function() {
   var question_request = new XMLHttpRequest();
 
   question_request.open("get", "http://localhost:9292/question/1");
   question_request.addEventListener("load", function(event){
-    question.innerHTML = question_request.responseText
+    question.innerHTML = question_request.responseText;
   });
 
   var choices_request = new XMLHttpRequest();
 
   choices_request.open("get", "http://localhost:9292/choices/1");
   choices_request.addEventListener("load", function(event){
-    choices.innerHTML = choices_request.responseText
+    choices.innerHTML = choices_request.responseText;
   });
 
   var correct_request = new XMLHttpRequest();
@@ -26,6 +27,8 @@ begin.addEventListener("click", function() {
   correct_request.addEventListener("load", function(event){
     correct_answer.innerHTML = correct_request.responseText;
   });
+
+  points.innerHTML = "You have " + points_count + " point(s).";
 
   question_request.send();
   choices_request.send();
@@ -39,9 +42,11 @@ enter.addEventListener("click", function() {
   var correct_answer = document.getElementById('correct_answer').innerHTML;
 
   if (user_response == correct_answer) {
-    question_result.innerHTML = "Correct!"
+    question_result.innerHTML = "Correct!";
+    points_count++;
+    points.innerHTML = "You have " + points_count + " point(s).";
   } else {
-    question_result.innerHTML = "Incorrect."
+    question_result.innerHTML = "Incorrect.";
   }
 
 
