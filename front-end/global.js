61e5a1937d3660cce403fe42b448f42ca3ea89e3
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-14 12:19:34 -0500

    Barebones erb is displaying first question and choices

--- front-end/global.js
+++ front-end/global.js
@@ -1,2 +1,40 @@
 window.onload = function(){
+
+var begin = document.getElementById('begin_button')
+
+begin.addEventListener("click", function() {
+  var qna_request = new XMLHttpRequest();
+
+  qna_request.open("GET", "http://localhost:8080/question_and_choices/1");
+  qna_request.addEventListener("load", function(event){
+    var the_qna_request = event.target;
+    alert(the_qna_request.responseText);
+    
+  });
+
+  qna_request.send();
+
+
+
+
+});
+
+
+// var request = new XMLHttpRequest();
+
+// request.open("GET", "http://localhost:8080/info");
+// // a request to the server running through http-server
+
+// request.addEventListener("load", function(event){
+//     var the_request = event.target;
+//     alert(the_request.responseText);
+//     // responseText is a built-in method for request objects.
+// });
+
+// request.send();
+
+
+
+
+
 };
--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -1,3 +1,8 @@
 MyApp.get "/" do
   erb :"home"
 end
+
+MyApp.get "/question_and_choices/:id" do
+  @question_object = Question.find_by_id(params[:id])
+  erb :"/question_and_choices"
+end
