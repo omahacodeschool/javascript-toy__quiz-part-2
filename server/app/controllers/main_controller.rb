dc0b92b86544a1262580c464ca7e015e09de6573
Kyle Sandage <ksandage08@gmail.com>
2016-03-14 11:53:23 -0500

    Kyle

--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -1,3 +1,25 @@
 MyApp.get "/" do
   erb :"home"
 end
+
+MyApp.get "/add/question" do
+  erb :"new_question"
+end
+
+MyApp.post "/add/question_to_database" do
+  #cool stuff
+  add = Question.new
+  add.content = params["question"]
+  add.save
+
+  redirect "/"
+end
+
+MyApp.get "/add/answers" do
+  erb :"new_answer"
+end
+
+MyApp.post "/add/answer_to_database" do
+  #cool stuff
+  redirect "/add/answers"
+end
--- server/app/models/answer.rb
+++ server/app/models/answer.rb
@@ -1,3 +1,7 @@
+# DB.define_column("answers", "content", "text")
+# DB.define_column("answers", "question_id", "integer")
+# DB.define_column("answers", "correct", "boolean")
+
 class Answer < ActiveRecord::Base
 
   # Returns a single Question object.
