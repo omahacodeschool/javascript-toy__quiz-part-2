2b52d8c1ae293702dea38ac5fb6f69b2e99ef1c6
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-15 09:24:11 -0500

    built html page, js page collecting first question and displaying it upon page load

--- front-end/global.js
+++ front-end/global.js
@@ -1,20 +1,17 @@
 window.onload = function(){
 
-  var button = document.getElementById("begin_button");
-
-  button.addEventListener("click", function(){
-
-    var first_question = new XMLHttpRequest();
-
+  var questions = document.getElementById("questions");
+  
+  var first_question = new XMLHttpRequest();
     first_question.open("get", "http://localhost:9292/1");
     
-    first_question.addEventListener("load", function(event){
-    var the_first_question = event.target;
-      var response = the_first_question.responseText;
-      prompt(response.split());
-    });
-
-
-    first_question.send();
+  first_question.addEventListener("load", function(event){
+  var the_first_question = event.target;
+    var response = the_first_question.responseText;
+    questions.innerHTML = response;
   });
+
+  first_question.send();
+
 };
+
