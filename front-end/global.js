5c19494e2430053dd42d78a27a0cb17bd638bd89
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-15 18:06:20 -0500

    got a response from the server

--- front-end/global.js
+++ front-end/global.js
@@ -1,2 +1,20 @@
+var myRequest =  new XMLHttpRequest();
+
+function self(event){
+  var the_request = event.target
+  prompt(the_request.responseText)
+}
+
+
+function getText(){
+  myRequest.open("GET", "http://localhost:9292/");
+  myRequest.send();
+  myRequest.addEventListener("load",self)
+}
+
 window.onload = function(){
+
+var button = document.getElementById("begin_button")
+button.addEventListener("click",getText);
+
 };
