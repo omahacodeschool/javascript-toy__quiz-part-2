96897dac8a83f746871718c49d5643a4544c8de4
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-15 10:25:23 -0500

    wip counter

--- front-end/global.js
+++ front-end/global.js
@@ -1,67 +1,64 @@
 window.onload = function(){
 
-  var count = 0;
+  var count = 1;
   var score = 0;
 
   var questions = document.getElementById("questions");
   
   //Load page question
-  var first_question = new XMLHttpRequest();
-    first_question.open("get", "http://localhost:9292/questions/1");
+  var question = new XMLHttpRequest();
+    question.open("get", "http://localhost:9292/questions/#{count}");
     
-  first_question.addEventListener("load", function(event){
-  var the_first_question = event.target;
-    var question_response = the_first_question.responseText;
+  question.addEventListener("load", function(event){
+  var the_question = event.target;
+    var question_response = the_question.responseText;
     questions.innerHTML = question_response;
   });
 
-  first_question.send();
+  question.send();
 
   //load page chioces
   var chioces = document.getElementById("chioces");
   
-  var answer_to_first_question = new XMLHttpRequest();
-    answer_to_first_question.open("get", "http://localhost:9292/answers/1");
+  var answer = new XMLHttpRequest();
+    answer.open("get", "http://localhost:9292/answers/#{count}");
     
-  answer_to_first_question.addEventListener("load", function(event){
-  var the_answer_to_first_question = event.target;
-    var answer_response = the_answer_to_first_question.responseText;
+  answer.addEventListener("load", function(event){
+  var the_answer = event.target;
+    var answer_response = the_answer.responseText;
     chioces.innerHTML = answer_response;
   });
 
-  answer_to_first_question.send();
+  answer.send();
 
   //next button
   var next = document.getElementById("next");
 
-  var first_question = new XMLHttpRequest();
-    first_question.open("get", "http://localhost:9292/questions/1");
-    
-  first_question.addEventListener("load", function(event){
-  var the_first_question = event.target;
-    var question_response = the_first_question.responseText;
-    questions.innerHTML = question_response;
-  });
-
-  first_question.send();
-
-  var chioces = document.getElementById("chioces");
-  
-  var answer_to_first_question = new XMLHttpRequest();
-    answer_to_first_question.open("get", "http://localhost:9292/answers/1");
-    
-  answer_to_first_question.addEventListener("load", function(event){
-  var the_answer_to_first_question = event.target;
-    var answer_response = the_answer_to_first_question.responseText;
-    chioces.innerHTML = answer_response;
-  });
-
-  answer_to_first_question.send();
-
-
   next.addEventListener("click", function(){
+
+    var question = new XMLHttpRequest();
+      question.open("get", "http://localhost:9292/questions/#{count}");
+      
+    question.addEventListener("load", function(event){
+    var the_question = event.target;
+      var question_response = the_question.responseText;
+      questions.innerHTML = question_response;
+    });
+
+    question.send();
+
+    var chioces = document.getElementById("chioces");
     
-     
+    var answer = new XMLHttpRequest();
+      answer.open("get", "http://localhost:9292/answers/#{count}");
+      
+    answer.addEventListener("load", function(event){
+    var the_answer = event.target;
+      var answer_response = the_answer.responseText;
+      chioces.innerHTML = answer_response;
+    });
+
+    answer.send(); count++;
   });
 
 };
