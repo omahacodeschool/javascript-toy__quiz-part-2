0bc171114bdc813b6d74473117956d4f2f835bda
Amy Wall <amymariewall@gmail.com>
2016-03-15 16:45:45 -0500

    Next question now appearing and answers being submitted and checked. Going to start working on a way to prevent a request object from being created after all questions have been loaded, and a final score report.

--- front-end/global.js
+++ front-end/global.js
@@ -51,10 +51,11 @@ function showOption(option_key, text){
 
 function submitAnswer() {
     var userAnswer = document.querySelector('input[name="option"]:checked').value;
+    debugger;
     var result = document.getElementById("question_result");
     //result.innerHTML = userAnswer;
     var check_answer_request = new XMLHttpRequest();
-    check_answer_request.open("GET", "http://localhost:9292/verify/1/" + userAnswer);
+    check_answer_request.open("GET", "http://localhost:9292/verify/" + question + "/" + userAnswer);
 
     check_answer_request.addEventListener("load", function(event){
       var the_check_answer_request = event.target;
@@ -63,8 +64,9 @@ function submitAnswer() {
       if (check_answer_request.responseText == "CORRECT!"){
         score++;
       }
-    })
+    });
     check_answer_request.send();
+    question++;
   };
 
   function showQuestion(){
@@ -88,73 +90,10 @@ window.onload = function(){
   var begin_button = document.getElementById("begin_button");
 
   begin_button.addEventListener("click", function(event) {
-
     showQuestion(event);
     createOptionsList(event);
-
-  //var question_request = new XMLHttpRequest();
-    //creates a new Request Object
-  //question_request.open("GET", "http://localhost:9292/question/" + question);
-    // specifies the path and http verb for the request, using the open method 
-  event.target.style.display = "none";
-
-  //question_request.addEventListener("load", function(event){
-    //var the_question_request = event.target;
-
-    //var question_element = document.getElementById("question");
-
-    //question_element.innerHTML = the_question_request.responseText;
-
-    //FIND ALTERNATIVE METHOD OF GETTING ANSWER
-    //var userAnswer = prompt(the_request.responseText);
-    // responseText is a built-in method for request objects.
-
-    //var request_answer = new XMLHttpRequest();
-
-    //request_answer.open("GET", "http://localhost:9292/check_answer/1/" + userAnswer);
-
-    //request_answer.addEventListener("load", function(event) {
-      //var answer_request = event.target;
-      //alert(answer_request.responseText);
-    //});
-
-    //request_answer.send();
-
-  
-
-  //question_request.send();
-
-  //var choices_request = new XMLHttpRequest();
-    // creates a new Request Object to get the choices
-
-    //choices_request.open("GET", "http://localhost:9292/choices/" + question);
-    //specifies the path and http verb for the choices request
-
-  // choices_request.addEventListener("load", function(event){
-  //   var the_choices_request = event.target;
-  //   var choices_element = document.getElementById("choices");
-  //   var options_string = the_choices_request.responseText;
-  //   var response_array = options_string.split("***");
-  //   var options_array = response_array.slice(1);
-
-    
-  //   for (i = 0; i < options_array.length; i++) {
-  //     var option = showOption(i, options_array[i]);
-  //     options_list.appendChild(option);
-  // }
-
-  //   choices_element.appendChild(options_list);
-
-
-
-  // });
-
-  // choices_request.send();
-
-});
-
-  
-
+    event.target.style.display = "none";
+  });
 
   var submitter = document.getElementById("submitter");
 
@@ -162,6 +101,14 @@ window.onload = function(){
     submitAnswer(submitter);
   });
   
+  var next = document.getElementById("next");
+
+  next.addEventListener("click", function(event){
+    var result_display = document.getElementById("question_result");
+    result_display.innerHTML = "";
+    showQuestion(event);
+    createOptionsList(event);
+  });
 
 
 
