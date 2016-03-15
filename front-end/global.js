e5028dabeb6082495879e04c10e0b32b7f133900
Chris Foral <chris.foral@gmail.com>
2016-03-15 11:56:32 -0500

    Set up iterator for requesting certain question and answer sets. Not sure if this is the intended setup, but it works

--- front-end/global.js
+++ front-end/global.js
@@ -1,11 +1,11 @@
 window.onload = function(){
 
   var start_it  = document.getElementById("begin_button");
-  var quizcount = 1
+  var quizCount = 1
 
   start_it.addEventListener("click", function() {
     var request = new XMLHttpRequest();
-    request.open("GET", "http://localhost:9292/question_answer_set/1");
+    request.open("GET", "http://localhost:9292/question_answer_set/" + quizCount + "");
 
     request.addEventListener("load", function(event) {
       var the_request = event.target;
