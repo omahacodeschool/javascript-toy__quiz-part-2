e0ab9ab35a7137ce52667125951f8e8cde1a6d8a
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-15 09:55:31 -0500

    load page displaying both question/answer chioces

--- front-end/global.js
+++ front-end/global.js
@@ -3,15 +3,28 @@ window.onload = function(){
   var questions = document.getElementById("questions");
   
   var first_question = new XMLHttpRequest();
-    first_question.open("get", "http://localhost:9292/1");
+    first_question.open("get", "http://localhost:9292/questions/1");
     
   first_question.addEventListener("load", function(event){
   var the_first_question = event.target;
-    var response = the_first_question.responseText;
-    questions.innerHTML = response;
+    var question_response = the_first_question.responseText;
+    questions.innerHTML = question_response;
   });
 
   first_question.send();
+
+  var chioces = document.getElementById("chioces");
+  
+  var answer_to_first_question = new XMLHttpRequest();
+    answer_to_first_question.open("get", "http://localhost:9292/answers/1");
+    
+  answer_to_first_question.addEventListener("load", function(event){
+  var the_answer_to_first_question = event.target;
+    var answer_response = the_answer_to_first_question.responseText;
+    chioces.innerHTML = answer_response;
+  });
+
+  answer_to_first_question.send();
 
 };
 
--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -1,7 +1,10 @@
-MyApp.get "/1" do
+MyApp.get "/questions/1" do
   @first_question = Question.find_by_id(1)
-  # @first_question.save
-  # @answers_to_first_question = Answer.where("question_id" => 1)
   erb :"home"
+end
+
+MyApp.get "/answers/1" do
+  @answers_to_first_question = Answer.where("question_id" => 1)
+  erb :"answer_index"
 end
 
