14690c4074b923af0cbd56d59e5d8998c7276f8c
Beth Haubert <bethany.haubert@gmail.com>
2016-03-15 10:04:13 -0500

    working on answer display

--- front-end/global.js
+++ front-end/global.js
@@ -2,15 +2,17 @@ window.onload = function(){
   var begin_button = document.getElementById("begin_button");
   begin_button.addEventListener("click", function(){
     var request = new XMLHttpRequest();
-    request.open("GET", "http://localhost:9292/combined_question_and_answer");
+    request.open("GET", "http://localhost:9292/combined_question_and_answer/1");
 
     request.addEventListener("load", function(event){
       var the_request = event.target
-      prompt(the_request.responseText);
+      var response = prompt(the_request.responseText);
     });
 
     request.send();
 
+  var next = new XMLHttpRequest();
+    next.open("GET", "http://localhost:9292/correct_answer/1/response");
 
 
   })
--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -2,7 +2,18 @@ MyApp.get "/" do
   erb :"home"
 end
 
-MyApp.get "/combined_question_and_answer" do
+MyApp.get "/combined_question_and_answer/:id" do
   @question = Question.first
   erb :"combined_question_and_answers"
+end
+
+MyApp.get "/correct_answer/:question/:user_answer" do
+  @question = Question.find_by(params[:question])
+  @user_answer = params[:user_answer]
+    erb :"correct_answer"
+  if @question.correct_answer == @user_answer
+    erb :"correct_answer"
+  else
+    erb :"wrong_answer"
+  end
 end
