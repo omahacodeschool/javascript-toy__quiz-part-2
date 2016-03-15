8362b04ad8693e3f84f2cc6ce22e5ba89d0b8cd7
Beth Haubert <bethany.haubert@gmail.com>
2016-03-15 10:15:28 -0500

    wrkng

--- front-end/global.js
+++ front-end/global.js
@@ -5,15 +5,24 @@ window.onload = function(){
     request.open("GET", "http://localhost:9292/combined_question_and_answer/1");
 
     request.addEventListener("load", function(event){
-      var the_request = event.target
+      var the_request = event.target;
       var response = prompt(the_request.responseText);
     });
 
     request.send();
+  });
 
-  var next = new XMLHttpRequest();
+
+  response.addEventListener("?", function(){
+    var next = new XMLHttpRequest();
     next.open("GET", "http://localhost:9292/correct_answer/1/response");
 
+    next.addEventListener("load", function(event){
+      var the_answer = event.target;
+      alert(the_answer.responseText);
+      });
 
-  })
+      next.send();
+    });
+
 };
