6a494089d1507806acfbaa900a8eb4b612e0cd2f
Chris Foral <chris.foral@gmail.com>
2016-03-15 16:22:57 -0500

    working on answer validation but running into an error

--- front-end/global.js
+++ front-end/global.js
@@ -1,21 +1,33 @@
 window.onload = function(){
 
-  var start_it  = document.getElementById("begin_button");
-  var quizCount = 1
+  var start_it      = document.getElementById("begin_button");
+  var questionCount = 1
+  var winCount      = 0
 
   start_it.addEventListener("click", function() {
     var request = new XMLHttpRequest();
-    request.open("GET", "http://localhost:9292/question_answer_set/" + quizCount + "");
+    request.open("GET", "http://localhost:9292/question_answer_set/" + questionCount + "");
 
     request.addEventListener("load", function(event) {
-      var the_request = event.target;
-      console.log("Question " + quizCount + ": " + the_request.responseText);
+      var question_request = event.target;
+      console.log("Question " + questionCount + ": " + question_request.responseText);
       
-      var userAnswer = prompt(the_request.responseText);
+      var userAnswer = prompt(question_request.responseText);
       console.log("User Answer: " + userAnswer);
-      quizCount++;
+      ;
+
+      var submit = new XMLHttpRequest();
+      submit.open("GET", "http://localhost:9292/answer_check/" + questionCount + "/" + userAnswer + "");
+
+      submit.addEventListener("load", function(event) {
+      var answer_request = event.target;
+
+      alert(answer_request.responseText);
+      console.log(answer_request.responseText);
+      });
+      submit.send();
     });
     request.send();
+    // questionCount++
   });
-
 };
--- server/app/controllers/question_controller.rb
+++ server/app/controllers/question_controller.rb
@@ -1,5 +1,13 @@
-MyApp.get "/question_answer_set/:id" do
-  @question = Question.find_by_id(params[:id])
+MyApp.get "/question_answer_set/:question_id" do
+  @question = Question.find_by_id(params[:question_id])
 
   erb :"question"
+end
+
+MyApp.get "/answer_check/:question_id/:user_answer" do
+  @question    = Question.find_by_id(params[:question_id])
+  @user_answer = params[:user_answer]
+  @corr_answer = @question.correct_answer
+
+  erb :"answer"
 end
