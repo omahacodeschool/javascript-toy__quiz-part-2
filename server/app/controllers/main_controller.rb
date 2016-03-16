2a252a4041ed527171ce32cb749db6551322f915
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-16 09:43:29 -0500

    figuring out how to process answer

--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -1,3 +1,12 @@
 MyApp.get "/" do
+  @question = Question.find_by_id(1)
+  @answer = Answer.where({question_id: 1})
   erb :"home"
+end
+
+Myapp.post "/answer" do
+  #some data comes in with form
+  #check to see if data matches answer in database
+  #assign boolean to varialbe
+  #erb response page should have if true send this if false send that  
 end
