2800e8cee23b1b429f1a2161cce9c31a566bab6c
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-16 13:51:45 -0500

    still cant figure this out

--- front-end/global.js
+++ front-end/global.js
@@ -1,13 +1,25 @@
 var myRequest =  new XMLHttpRequest();
 var button = document.getElementById("begin_button");
-var users_response = answer.value;
+var answer = "";
 
 //Creates a variable for the event itself
 //calls the response text from variable
 //stores response text in answer
 function self(event){
   var the_request = event.target
+  //save prompt answer as 'answer'
+
   answer = prompt(the_request.responseText)
+
+  //how do i send answer back to the server? in the URL?
+  //but if i do that isnt answer just a string and not a value?
+  myRequest.open("GET", "http://localhost:9292/answer");
+  
+  //send the request
+  myRequest.send();
+
+  //can i call the same function inside of itself?
+  myRequest.addEventListener("load",self)
 }
 
 //opens a new XHR request
@@ -23,5 +35,6 @@ window.onload = function(){
 
 button.addEventListener("click",getText);
 
-var
+
+
 };
