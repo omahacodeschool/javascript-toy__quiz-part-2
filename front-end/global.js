62110569710c845cc72527d1b3d34f639907eb19
Michael Meier <mmeier61@yahoo.com>
2016-03-15 22:26:34 -0500

    now

--- front-end/global.js
+++ front-end/global.js
@@ -1,43 +1,116 @@
 window.onload = function(){
-var click = document.getElementById('begin_button');
+var choices = document.getElementById('choices');
+var question = document.getElementById('question');
+var score = 0;
 var params = 1;
-var userData = "";
+var click_it = document.getElementById('begin_button');
+var submitter = document.getElementById('submitter');
+var next = document.getElementById('next');
+var result = document.getElementById('result');
+var submitter = document.getElementById('submitter'); 
+var total = document.getElementById('totalResult');
+var answer = document.getElementById('answer');
+var quiz = document.getElementById('quiz');
+var questionResult = document.getElementById('questionResult');
 
 
-  click.addEventListener("click", function(){
-    var request = new XMLHttpRequest();
-  request.open("GET", "http://localhost:9292/quiz/" + params);
-  request.addEventListener("load", function(event){
-    var the_request = event.target;
-  userData = prompt(the_request.responseText);
-    var answer = new XMLHttpRequest();
 
-  answer.open("GET", "http://localhost:9292/verify/" + params + "/" + userData);
-    answer.addEventListener("load", function(event){
-        var verify = event.target;
-        alert(verify.responseText);
+  click_it.addEventListener("click", function() {
+    click_it.style.display = "none";
+    quiz.style.display = "block";
+    submitter.style.display = "block";
+  
+    var questionBack = new XMLHttpRequest();
+    questionBack.open("get", "http://localhost:9292/quiz/"+params);
+    questionBack.addEventListener("load", function(event){
+    question.innerHTML = questionBack.responseText;
     });
-    answer.send();      
+
+    var choiceBack = new XMLHttpRequest();
+    choiceBack.open("get", "http://localhost:9292/choices/"+params);
+    choiceBack.addEventListener("load", function(event){
+    choices.innerHTML = choiceBack.responseText;
     });
-    request.send();
+
+    var answerBack = new XMLHttpRequest();
+    answerBack.open("get", "http://localhost:9292/answer/"+params);
+    answerBack.addEventListener("load", function(event){
+    correct.innerHTML = answerBack.responseText;
+    });
+
+
+    questionBack.send();
+    choiceBack.send();
+    answerBack.send();
+
+  });
+
+  submitter.addEventListener("click", function() {
+    submitter.style.display = "none";
+    next.style.display = "block";
+    questionResult.style.display = "block";
+    result.style.display = "block";
+      
+
+    var userData = answer.value;
+    var correct = document.getElementById('correct').innerHTML;
+    var question = document.getElementById('question').innerHTML;
+    
+
+    if (userData == correct) {
+      questionResult.innerHTML = "Correct!";
+      score++;
+      result.innerHTML = "Your Score is: " + score;
+      } 
+    else if (userData == "") {
+      questionResult.innerHTML = "Invalid Response.";
+      submitter.style.display = "block";
+      next.style.display = "none";
+      } 
+    else {
+    questionResult.innerHTML = "Nope, the question: " + question + " would be better described as: " + correct ;
+    }
+
+    if (params >= 4 && userData != "") {
+    submitter.style.display = "none";
+    next.style.display = "none";
+    result.innerHTML = "You earned a score of " + score + " out of 4 points, " + " or " + (score/4)*100 + "%.";
+    }
+  });
+
+  next.addEventListener("click", function() {
+    next.style.display = "none";
+    submitter.style.display = "block";
+    questionResult.style.display = "none";
+    params++;
+    answer.value = "";
+
+    var questionBack = new XMLHttpRequest();
+
+    questionBack.open("get", "http://localhost:9292/quiz/"+params);
+    questionBack.addEventListener("load", function(event){
+    question.innerHTML = questionBack.responseText;
+    });
+
+    var choiceBack = new XMLHttpRequest();
+
+    choiceBack.open("get", "http://localhost:9292/choices/"+params);
+    choiceBack.addEventListener("load", function(event){
+    choices.innerHTML = choiceBack.responseText;
+    });
+
+    var answerBack = new XMLHttpRequest();
+
+    answerBack.open("get", "http://localhost:9292/answer/"+params);
+    answerBack.addEventListener("load", function(event){
+    correct.innerHTML = answerBack.responseText;
+    });
+
+    result.innerHTML = "You earned " + score + " points.";
+
+    questionBack.send();
+    choiceBack.send();
+    answerBack.send();
   });
 };
-
-   
-  // });
-  // var the_request = event.target
-  //gives a variable to the instance of the request, (event.self)
-  // alert(the_request.responseText);
-  //alert(variable given to instance of event(.) responseText)
-
-// request.send();
-// });
-
-
-//sending the request to the controller
-
-// }
-
-
-
-
+  
