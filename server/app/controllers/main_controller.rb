fba2d6201b8604197663beb0aa0a278402006e1a
Thorin Butler <thorin.butler@gmail.com>
2016-03-20 19:37:52 -0500

    Trying to wade back into this again.  I feel like I know what needs to be done in the broad strokes, but the actual details seem oddly elusive.

--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ MyApp.get "/" do
   erb :"home"
 end
 
-MyApp.get "/first_question_and_answer_set" do
+MyApp.get "/combined_question_and_answer/:id" do
   @question = Question.first
   erb :"questions/question_and_answer_set"
 end
