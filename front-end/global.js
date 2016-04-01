14af742582aa2220f003ff6bbd04d9748b4cd138
Thorin Butler <thorin.butler@gmail.com>
2016-04-01 15:53:34 -0500

    this thing is annoying me

--- front-end/global.js
+++ front-end/global.js
@@ -7,25 +7,14 @@ window.onload = function(){
     var request = new XMLHttpRequest();
 
     request.open("GET", "http://localhost:9292/combined_question_and_answer/1");
-
+    
     request.send();
 
     request.addEventListener("load", function(event){
       var ua = event.target.response
 
       document.getElementById("question").innerHTML = ua
+
     });
-  });  
-  
-  var submit = document.getElementById("submitter");
-  
-  request.open("GET", "http://localhost:9292/combined_question_and_answer/1");
-
-  request.send();
-
-  request.addEventListener("load", function(event){
-    var ca = event.target.response
-
-    document.getElementById("question_result").innerHTML = ca
-  });  
+  });
 }; 
